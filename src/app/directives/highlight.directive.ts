9e498a86b99c0dab9f7428ef3711ff7d49036466
Surekha Shelake <shelakesurekha@gmail.com>
2022-01-03 10:01:48 +0530

    added event handling on custom directives

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,14 +1,28 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[yujHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(myelem : ElementRef) {
-    myelem.nativeElement.style.backgroundColor = 'yellow';
-    myelem.nativeElement.style.fontSize = '30px';
+  constructor(private myelem: ElementRef) {
+    // myelem.nativeElement.style.backgroundColor = 'yellow';
+
   }
 
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.highlight('yellow', '30px');
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.highlight('', '12px');
+  }
+
+  private highlight(color: string, fontSize: string) {
+    this.myelem.nativeElement.style.backgroundColor = color;
+    this.myelem.nativeElement.style.fontSize = fontSize;
+  }
 
 }
