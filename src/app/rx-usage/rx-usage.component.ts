0d777ebe05ebdf69a2e6cd97f9a455440273ab1a
Surekha Shelake <shelakesurekha@gmail.com>
2022-01-05 09:29:37 +0530

    added bhevious subject, subject

--- src/app/rx-usage/rx-usage.component.ts
+++ src/app/rx-usage/rx-usage.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { interval, map, Observable, take, tap, of, first, last, range, concat, filter, merge, mergeMap } from 'rxjs';
 
+import { Subject, AsyncSubject, BehaviorSubject, ReplaySubject } from 'rxjs';
 
+// import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'yuj-rx-usage',
@@ -25,7 +27,12 @@ export class RxUsageComponent implements OnInit {
     //   console.log(v)
     // });
 
-    this.rxJsOperatos();
+    //this.rxJsOperatos();
+
+    // this.rxJsSubjects();
+
+    // this.onlySubjectAndBehaviorSubject();
+    this.normalObservalble();
   }
 
   rxJsOperatos() {
@@ -105,5 +112,105 @@ export class RxUsageComponent implements OnInit {
 
   }
 
+  rxJsSubjects() {
 
+    const subject = new Subject();
+    const asyncSubject = new AsyncSubject();
+    const behaviorSubject = new BehaviorSubject('a');
+    const replaySubject = new ReplaySubject(2);
+
+    const subjects = [subject, behaviorSubject];
+    const log = (subjectType: any) => (e: any) => console.log(`${subjectType}: ${e}`);
+
+    console.log('SUBSCRIBE 1');
+    subject.subscribe(log('s1 subject'));
+    //asyncSubject.subscribe(log('s1 asyncSubject'));
+    behaviorSubject.subscribe(log('s1 behaviorSubject'));
+    // replaySubject.subscribe(log('s1 replaySubject'));
+
+    console.log('\nNEXT(r)');
+    subjects.forEach(o => o.next('r'));
+
+    console.log('\nNEXT(x)');
+    subjects.forEach(o => o.next('x'));
+
+    console.log('\nSUBSCRIBE 2');
+    subject.subscribe(log('s2 subject'));
+    // asyncSubject.subscribe(log('s2 asyncSubject'));
+    behaviorSubject.subscribe(log('s2 behaviorSubject'));
+    // replaySubject.subscribe(log('s2 replaySubject'));
+
+    console.log('\nNEXT(j)');
+    subjects.forEach(o => o.next('j'));
+
+
+
+    console.log('\nNEXT(s)');
+    subjects.forEach(o => o.next('s'));
+
+    subject.subscribe(log('s3 subject'));
+    // asyncSubject.subscribe(log('s2 asyncSubject'));
+    behaviorSubject.subscribe(log('s3 behaviorSubject'));
+
+    console.log('\nCOMPLETE');
+    subjects.forEach(o => o.complete());
+  }
+
+  onlySubjectAndBehaviorSubject() {
+    let bSubject = new BehaviorSubject("a");
+
+    bSubject.next("b");
+
+    bSubject.subscribe(value => {
+      console.log("bSubject - Subscription got", value); // Subscription got b, 
+      // ^ This would not happen 
+      // for a generic observable 
+      // or generic subject by default
+    });
+
+    bSubject.next("c"); // Subscription got c
+    bSubject.complete();
+    bSubject.next("d"); // Subscription got d
+
+    // Example 2 with regular subject:
+
+    // Regular Subject
+
+    let subject = new Subject();
+
+    subject.next("sb");
+
+    subject.subscribe(value => {
+      console.log("Subject - Subscription got", value); // Subscription wont get 
+      // anything at this point
+    });
+
+    subject.next("sc"); // Subscription got c
+    subject.complete();
+    subject.next("sd"); // Subscription got d
+  }
+
+  normalObservalble() {
+    let myLocation = new Observable((observer) => {
+      let watchId: number;
+      // Simple geolocation API check provides values to publish
+      if ('geolocation' in navigator) {
+        watchId = navigator.geolocation.watchPosition(
+          (position: GeolocationPosition) => {
+            observer.next(position);
+            observer.complete();
+          },
+          (error: GeolocationPositionError) => {
+            observer.error(error);
+          });
+      } else {
+        observer.error('Geolocation not available');
+      }
+
+    });
+
+    myLocation.subscribe((ms) => console.log(ms))
+  }
 }
+
+
