3e9770c9fcfdb864142e17781956522b938ed9db
Surekha Shelake <shelakesurekha@gmail.com>
2022-01-12 09:40:47 +0530

    added can activite route guard in app

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 export interface GuideUser {
@@ -12,12 +12,32 @@ export interface GuideUser {
 export class AuthGuard implements CanActivate, GuideUser {
   name: string; //rule effect becuase guideuser
 
+  constructor(private router: Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    alert(1);
-    return true;
+    console.log(route)
+
+    let details = localStorage.getItem('loginInfo');
+
+    // if (details && Object.keys(details).length > 0) { //user is logged or not
+    //   this.router.navigate(['/templates']);
+    //   return true;
+    // }
+
+    // this.router.navigate(['/login']);
+    // return false;
+
+
+
+    if (route.data && route.data["role"] !== 'admin') {
+      return false;
+    }
+
+    return true
 
   }
 
--- src/app/main-routing.module.ts
+++ src/app/main-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EmailTemplatesComponent } from './email-templates/email-templates.component';
+import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
@@ -12,7 +13,13 @@ import { UsersComponent } from './users/users.component';
 const appRoutes: Routes = [
   {
     path: 'dashboard',
-    component: DashboardComponent
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+    data: {
+      role: 'admin',
+      title: 'This only for user',
+      anyData: []
+    }
   },
   {
     path: 'login',
@@ -24,7 +31,8 @@ const appRoutes: Routes = [
   },
   {
     path: 'ticket',
-    component: SupportTicketComponent
+    component: SupportTicketComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'templates',
@@ -32,7 +40,8 @@ const appRoutes: Routes = [
   },
   {
     path: 'users',
-    component: UsersComponent
+    component: UsersComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: '',
